b313a890ed79cf02e97b4494fef1936eb21eb2f1
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-05-13 15:29:31 -0300

    logica finalizada

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,29 @@ import Cabecalho from './components/Cabecalho.vue';
 import { reactive } from 'vue';
 
 const estado = reactive({
-  numero1: '',
-  numero2: '',
+  numero1: 0,
+  numero2: 0,
   operacao: 'soma',
 })
 
+const realizaCalculo = () => {
+  const numero1 = parseFloat(estado.numero1);
+  const numero2 = parseFloat(estado.numero2);
+  const operacao = estado.operacao;
+
+  switch (operacao) {
+    case 'soma':
+      return numero1 + numero2;
+    case 'subtracao':
+      return numero1 - numero2;
+    case 'multiplicacao':
+      return numero1 * numero2;
+    case 'divisao':
+      return numero1 / numero2;
+    default:
+      return 0;
+  }
+}
 
 </script>
 
@@ -17,10 +35,10 @@ const estado = reactive({
     <div class="formulario">
       <div class="row mb-4">
         <div class="col-4">
-          <input type="number" class="form-control border-2" placeholder="Numero 1">
+          <input @change="evento => estado.numero1 = evento.target.value" type="number" class="form-control border-2" placeholder="Numero 1">
         </div>
         <div class="col-md-1">
-          <select class="form-select text-center">
+          <select @input="evento => estado.operacao = evento.target.value" class="form-select text-center">
             <option value="soma">+</option>
             <option value="subtracao">-</option>
             <option value="multiplicacao">x</option>
@@ -28,12 +46,12 @@ const estado = reactive({
           </select>
         </div>
         <div class="col-md-4">
-          <input type="number" class="form-control" placeholder="Numero 2">
+          <input @input="evento => estado.numero2 = evento.target.value" type="number" class="form-control" placeholder="Numero 2">
         </div>
       </div>
 
       <div class="row">
-        <span class="resultado">Resultado: 45</span>
+        <span class="resultado">Resultado: {{ realizaCalculo() }}</span>
       </div>
     </div>
   </div>
